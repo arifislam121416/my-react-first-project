6d74fe58bc989cfa642803ffc5cea01eeff6d371
arifislam121416 <ardairyhelthcare@gmail.com>
2026-03-14 23:06:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Countries from './component/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+    <Countries></Countries>
+      <section id="center">
+        
+        <div>
+          <h1>Get Start React Project</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+
+      
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Al Amin</h1>
+        </div>
+    );
+};
+
+export default Countries;
