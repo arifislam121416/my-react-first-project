f666c9ed543f78a6974ed02f161eae3ba3c1c265
arifislam121416 <ardairyhelthcare@gmail.com>
2026-03-15 13:22:48 +0600

    some add

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({country}) => {
+    console.log(country);
     return (
-        <div>
-            
+        <div className='cards'>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common} </h3>
+            <h3>Official: {country.name.official} </h3>
+            <p>Capital: {country.capital.capital}</p>
+           <h2>Region: {country.region.region} </h2>
+           <h3>population: {country.population.population} </h3>
         </div>
     );
 };
